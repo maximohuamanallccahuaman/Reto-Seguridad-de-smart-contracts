8463337c07989afb5061a0aa260734ba21f34faf
Máximo Huamán Allccahuamán <72884594+maximohuamanallccahuaman@users.noreply.github.com>
2022-12-01 18:54:48 -0500

    Update reto.sol

--- reto.sol
+++ reto.sol
@@ -2,25 +2,37 @@
 pragma solidity ^0.8.0;
 
 contract Desafio {
-    uint private pin;
+    uint private pin; // Nada es privado en la blockchain
+    address private owner;
     mapping(address => uint) balances;
 
     constructor(uint ownerPin) {
+        owner = msg.sender;
         pin = ownerPin;
     }
 
-    function min(uint ownerPin, uint amount) public {
+    // Cualquiera puede ejecutarlo
+    // El amount no está siendo controlado
+    function min(uint ownerPin, uint amount) public onlyowner{
         require(pin == ownerPin, "El pin no es correcto");
         balances[msg.sender] += amount;
     }
 
-    function depositar() public payable{
+    function depositar() public payable {
         balances[msg.sender] += msg.value;
     }
 
+    
     function retirar() public {
-        require(balances[msg.sender] > 0);
-        msg.sender.call{value:balances[msg.sender]}("");
-        balances[msg.sender] = 0;
+        require(balances[msg.sender] > 0, "Insufficient balance");
+        balances[msg.sender] = 0; // No te tenemos garantia que se seteara a 0
+        (bool success, ) = payable(msg.sender).call{value:balances[msg.sender]}(""); // No hay limite de gas, no estamos controlando el succes
+        require(success, "Error al enviar eth");
+    }
+    
+
+    modifier onlyowner() {
+        require(owner == msg.sender, "Solo el propietario puede actualizar los terminos");
+        _;
     }
 }
